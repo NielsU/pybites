367a3cd632e1c4b5616d808f8a06a32b663214f4
NielsU <NielsU@users.noreply.github.com>
2024-07-07 20:54:08 +0200

    separate chars with dictionary to remeber original char index.

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,12 +1,22 @@
 def get_index_different_char(chars):
     alpha = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
-    # Get indexes by filtering through list comprehention, the index of odd char is in list of len 1.
-    alpha_idx = [i for i, char in enumerate(chars) if char in alpha]
-    non_alpha_idx = [i for i, char in enumerate(chars) if char not in alpha]
+    # Devide alphanumeric chars from non alpha chars in separate dictionaries linked to their original index
+    # The char index is used as key as those are unique.
+    alpha_dict = {idx: char for idx, char in enumerate(chars) if str(char) in alpha}
+    non_alpha_dict = {
+        idx: char for idx, char in enumerate(chars) if str(char) not in alpha
+    }
 
-    # Return the index matching the odd char.
+    # List the keys for both dictionaries,which are the indexes
+    alpha_idx = list(alpha_dict.keys())
+    non_alpha_idx = list(non_alpha_dict.keys())
+
+    # The list with length 1 is the odd char, return its value being the index
     if len(alpha_idx) == 1:
         return alpha_idx[0]
     else:
         return non_alpha_idx[0]
+
+
+print(get_index_different_char([1, "=", 3, 4, 5, "A", "b", "a", "b", "c"]))  # prints 1
