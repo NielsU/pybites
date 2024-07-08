22fe6548f36035efb1e23af3b0240c06e95e9c89
NielsU <NielsU@users.noreply.github.com>
2024-07-08 09:26:24 +0200

    use _ for local members/properties

--- 19/simple_property.py
+++ 19/simple_property.py
@@ -5,9 +5,9 @@ NOW = datetime.now()
 
 class Promo:
     def __init__(self, name: str, expires: datetime):
-        self.name = name
-        self.expires = expires
+        self._name = name
+        self._expires = expires
 
     @property
     def expired(self) -> bool:
-        return self.expires < NOW
+        return self._expires < NOW
