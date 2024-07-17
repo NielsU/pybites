59f9e18b2fa7e5eca36139f21103605ed9aee434
NielsU <NielsU@users.noreply.github.com>
2024-07-17 12:05:26 +0200

    updated comment

--- 270/freq.py
+++ 270/freq.py
@@ -1,6 +1,6 @@
 """
     solution idea's:
-        1 - Default dict as counter, return (first item in) list of keys sorted by max value. (loops through entire list)
+        1 - default dict as counter, return (first item in) list of keys sorted by max value. (loops through entire list)
         2 - same as 1 but remove the counted values from list so that looping through entire number is not needed. 
         3 - count same as previous but create new view (map) of prevous map excluding the counted number?
         4 - ah!! collections counter! :) 
