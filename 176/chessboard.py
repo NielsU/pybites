bc79d4aa52b792893d04003d46a8fe5dc8374abe
NielsU <NielsU@users.noreply.github.com>
2024-07-17 09:28:32 +0200

    Adds comment reflecting on the solution

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -1,3 +1,20 @@
+"""
+:) This bite reminded me to model things as they really are, 
+that the perspective on the problem to solve makes the difference.
+
+My mind was on printing rows, not on the actual thing, constructing a board.
+After building my solution i realise it works only for even sizes.
+
+I added some logic to append a char depening on size being even.
+Looked at the code and thought well, the tests dont require it. 
+Code is now more complicated. lets not be perfectionistic to add unneeded logic. So i removed it.
+
+Than seeing the provided solution  (adding the char based on position on the board in nested for loop),
+i realised if i had had the perspective of constructing a board,
+i may have ended up building it with support for odd size without even thinking about it,
+and the solution would be just as simple. :).
+"""
+
 WHITE, BLACK = " ", "#"
 
 
