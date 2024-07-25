f69a1d8a95274f130514d6e015297020b268c2b6
NielsU <NielsU@users.noreply.github.com>
2024-07-25 14:57:46 +0200

    adds test using readout with capfd. Seems to work similar

--- 246/test_workouts.py
+++ 246/test_workouts.py
@@ -40,3 +40,33 @@ def test_print_workout_days(capsys, input, expected):
     captured = capsys.readouterr()
 
     assert captured.out == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ("Upper Body #1", "Mon\n"),
+        ("Lower Body #1", "Tue\n"),
+        ("30 min cardio", "Wed\n"),
+        ("upper body #2", "Thu\n"),
+        ("lower body #2", "Fri\n"),
+        ("Upper", "Mon, Thu\n"),
+        ("upper", "Mon, Thu\n"),
+        ("lower", "Tue, Fri\n"),
+        ("Body", "Mon, Tue, Thu, Fri\n"),
+        ("#", "Mon, Tue, Thu, Fri\n"),
+        ("30", "Wed\n"),
+        ("min", "Wed\n"),
+        ("cardio", "Wed\n"),
+        ("1", "Mon, Tue\n"),
+        ("2", "Thu, Fri\n"),
+        (" ", "Mon, Tue, Wed, Thu, Fri\n"),
+        ("notAmatch", "No matching workout\n"),
+        ("", "Mon, Tue, Wed, Thu, Fri\n"),
+    ],
+)
+def test_print_workout_days_capfd(capfd, input, expected):
+    print_workout_days(input)
+    captured = capfd.readouterr()
+
+    assert captured.out == expected
