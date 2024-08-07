12223685176d60f5c0a2327ced993205753bcebc
NielsU <NielsU@users.noreply.github.com>
2024-08-07 08:40:25 +0200

    submit 120 validator decorator.

--- 120/validate.py
+++ 120/validate.py
@@ -3,4 +3,13 @@ from functools import wraps
 
 def int_args(func):
     @wraps(func)
-    # complete this decorator
+    def validate_args_int(*args, **kwargs):
+        # complete this decorator
+        for value in args:
+            if type(value) is not int:
+                raise TypeError
+            if int(value) < 0:
+                raise ValueError
+        return func(*args, **kwargs)
+
+    return validate_args_int
