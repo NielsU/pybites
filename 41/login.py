3fc630355ce09e86a771efa284a92e2a897a0d60
NielsU <NielsU@users.noreply.github.com>
2024-08-07 07:58:12 +0200

    added *args, **kwargs again, misread the solution, i figure now that user positional argument is used directly but other positional and keyword arguments are passed through. Would i think prevent breaking the code if the wrapped function is extended with additional arguments. :).

--- 41/login.py
+++ 41/login.py
@@ -16,12 +16,12 @@ See also the tests for more details. Have fun and enjoy!
 
 def login_required(func):
     @wraps(func)
-    def wrapper(user):
+    def wrapper(user, *args, **kwargs):
         if user not in known_users:
             return "please create an account"
         if user not in loggedin_users:
             return "please login"
-        return func(user)
+        return func(user, *args, **kwargs)
 
     return wrapper
 
