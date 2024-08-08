52ab210cc8aa1de067ef7d372ec5bed9655c11cd
NielsU <NielsU@users.noreply.github.com>
2024-08-08 09:34:58 +0200

    submit 279

--- 279/armstrong.py
+++ 279/armstrong.py
@@ -1,2 +1,23 @@
 def is_armstrong(n: int) -> bool:
-    # your code ...
+    # initialize sum
+    sum = 0
+    power = len(str(n))
+
+    for digit in str(n):
+        sum += int(digit) ** power
+
+    return sum == n
+
+
+def is_armstrong1(n: int) -> bool:
+    # initialize sum
+    sum = 0
+    power = len(str(n))
+
+    temp = n
+    while temp > 0:
+        digit = temp % 10
+        sum += digit**power
+        temp //= 10
+
+    return sum == n
