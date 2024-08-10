568c2dfcd25373bba44ea82cdd09a1e9f77c91c9
NielsU <NielsU@users.noreply.github.com>
2024-08-10 11:55:32 +0200

    some alternative solutions

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -1,24 +1,31 @@
 import itertools
 
 
+# helper function for calculating average of a list
+def rounded_avg(values: list[int]) -> float:
+    return round(sum(values) / len(values), 2)
+
+
 # Solution with creating slices, did not have to use isslice i think.
 def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
     returns a sequence of same length with the averages.
     You can assume all items in sequence are numeric."""
 
-    def rounded_avg(sequence):
-        return round(sum(sequence) / len(sequence), 2)
-
     running_mean = []
-    for i, _ in enumerate(sequence, start=1):
+    for i in range(1, len(sequence) + 1):
         slice = list(itertools.islice(sequence, i))
         running_mean.append(rounded_avg(slice))
     return running_mean
 
 
-# solution using accumulate. Think this is more elegant.
+# rewritten to a list comprehension.
 def running_mean2(sequence):
+    return [rounded_avg(sequence[:i]) for i in range(1, len(sequence) + 1)]
+
+
+# solution using accumulate. I think this is more opague/ diffcult to understand.
+def running_mean3(sequence):
     # get accummulated values in a list.
     accum = itertools.accumulate(sequence)
 
