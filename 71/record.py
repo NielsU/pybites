24d78f4d3df7ccdb6d88abaa295068a5d9f79e03
NielsU <NielsU@users.noreply.github.com>
2024-08-26 16:50:27 +0200

    submit 71

--- 71/record.py
+++ 71/record.py
@@ -1,3 +1,10 @@
-class RecordScore():
+class RecordScore:
     """Class to track a game's maximum score"""
-    pass
+
+    def __init__(self):
+        self._scores = []
+
+    def __call__(self, score: int) -> int:
+        self._scores.append(score)
+        self._scores.sort()
+        return self._scores[-1]
