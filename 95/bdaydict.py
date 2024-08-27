0f1593b3cb8783d9732791a99491fbe4f1e20b53
NielsU <NielsU@users.noreply.github.com>
2024-08-27 14:31:16 +0200

    submit 95

--- 95/bdaydict.py
+++ 95/bdaydict.py
@@ -1,9 +1,21 @@
-MSG = 'Hey {}, there are more people with your birthday!'
+from datetime import date
+
+MSG = "Hey {}, there are more people with your birthday!"
 
 
 class BirthdayDict(dict):
     """Override dict to print a message every time a new person is added that has
-       the same birthday (day+month) as somebody already in the dict"""
+    the same birthday (day+month) as somebody already in the dict"""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._birthdays = set()
 
     def __setitem__(self, name, birthday):
-        pass
+
+        if (birthday.day, birthday.month) in self._birthdays:
+            print(MSG.format(name))
+
+        self._birthdays.add((birthday.day, birthday.month))
+
+        super().__setitem__(name, birthday)
