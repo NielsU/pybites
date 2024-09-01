2cf3b476e7c9ff1bf744c29f374e67e835da80a8
NielsU <NielsU@users.noreply.github.com>
2024-09-01 16:38:02 +0200

    solution using itertools count to create sequence instead of writng one myself. :)

--- 138/zoo.py
+++ 138/zoo.py
@@ -1,20 +1,18 @@
-class Animal:
-    sequence = 10000
-    animals = []
+from itertools import count
 
-    @classmethod
-    def next_id(cls) -> int:
-        cls.sequence += 1
-        return cls.sequence
+
+class Animal:
+    _sequence = count(10001)
+    _animals = []
 
     def __init__(self, name):
         self.name = str(name).title()
-        self.id = self.next_id()
-        self.animals.append(self)
+        self.id = next(self._sequence)
+        self._animals.append(self)
 
     def __str__(self):
         return f"{self.id}. {self.name}"
 
     @classmethod
     def zoo(cls):
-        return "\n".join(str(animal) for animal in cls.animals)
+        return "\n".join(str(animal) for animal in cls._animals)
