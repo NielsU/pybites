fc300fae6f2f128e5d3b37c1d6d07efe60c509e5
NielsU <NielsU@users.noreply.github.com>
2024-09-26 11:30:47 +0200

    submit 111

--- 111/ipinfo.py
+++ 111/ipinfo.py
@@ -1,9 +1,12 @@
-import requests
+import requests, json
 
-IPINFO_URL = 'http://ipinfo.io/{ip}/json'
+IPINFO_URL = "http://ipinfo.io/{ip}/json"
 
 
 def get_ip_country(ip_address):
     """Receives ip address string, use IPINFO_URL to get geo data,
-       parse the json response returning the country code of the IP"""
-    pass
+    parse the json response returning the country code of the IP"""
+
+    response = requests.get(url=IPINFO_URL.format(ip=ip_address))
+
+    return dict(response.json())["country"]
