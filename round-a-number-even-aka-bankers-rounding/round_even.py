40615baad7122c0652a251b76582b8d8955dbe58
NielsU <NielsU@users.noreply.github.com>
2024-10-16 17:11:46 +0200

    simplified round already uses half even by default.

--- round-a-number-even-aka-bankers-rounding/round_even.py
+++ round-a-number-even-aka-bankers-rounding/round_even.py
@@ -3,15 +3,19 @@ import math
 
 def round_even(number):
     """Takes a number and returns it rounded even"""
-    result = 0
-    # Only x.5 numbers get the "special" treatment.
-    if number % 2 == 0.5:
-        # return the nearest even integer
-        result = (
-            math.floor(number) if math.floor(number) % 2 == 0 else math.ceil(number)
-        )
+    # result = 0
+    # # Only x.5 numbers get the "special" treatment.
+    # if number % 2 == 0.5:
+    #     # return the nearest even integer
+    #     result = (
+    #         math.floor(number) if math.floor(number) % 2 == 0 else math.ceil(number)
+    #     )
 
-    else:
-        result = round(number, 0)
+    # else:
+    #     result = round(number, 0)
 
-    return result
+    # return result
+
+    # turns out python 3 rounding is already half_even.
+    # https://docs.python.org/3/library/functions.html#round
+    return round(number, 0)
