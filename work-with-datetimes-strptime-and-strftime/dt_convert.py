f8c4dc4d5f952895bd530dd2c0c01a6a451d47f3
NielsU <NielsU@users.noreply.github.com>
2024-10-18 11:39:26 +0200

    submit strptime strftime bite + rename v1 bite folder.

---
+++ work-with-datetimes-strptime-and-strftime/dt_convert.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+THIS_YEAR = 2018
+
+
+def years_ago(date):
+    """Receives a date string of 'DD MMM, YYYY', for example: 8 Aug, 2015
+    Convert this date str to a datetime object (use strptime).
+    Then extract the year from the obtained datetime object and subtract
+    it from the THIS_YEAR constant above, returning the int difference.
+    So in this example you would get: 2018 - 2015 = 3"""
+    DATE_FORMAT = r"%d %b, %Y"
+
+    date_obj = datetime.strptime(date, DATE_FORMAT)
+    return THIS_YEAR - date_obj.year
+
+
+def convert_eu_to_us_date(date):
+    """Receives a date string in European format of dd/mm/yyyy, e.g. 11/03/2002
+    Convert it to an American date: mm/dd/yyyy (in this case 03/11/2002).
+    To enforce the use of datetime's strptime / strftime (over slicing)
+    the tests check if a ValueError is raised for invalid day/month/year
+    ranges (no need to code this, datetime does this out of the box)"""
+    EU_FORMAT = r"%d/%m/%Y"
+    US_FORMAT = r"%m/%d/%Y"
+
+    date_obj = datetime.strptime(date, EU_FORMAT)
+    return datetime.strftime(date_obj, US_FORMAT)
